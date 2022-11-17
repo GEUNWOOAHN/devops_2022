362d2332b03bf407da112a0d00e5548a3a2c6c30
CJ <CJ@host.docker.internal>
2022-11-17 14:07:53 +0900

    [gwahn] 배포

--- demo/src/main/java/com/example/demo/controller/ToDoController.java
+++ demo/src/main/java/com/example/demo/controller/ToDoController.java
@@ -5,8 +5,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestPart;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.entity.ContentDTO;
@@ -35,8 +35,8 @@ public class ToDoController {
      * @param 
      * @return 
      */
-    @PostMapping(value = "/write", consumes = {"application/json"})
-    public ResponseEntity<ContentDTO> save(@RequestBody ContentDTO contentDTO) {
+    @PostMapping(value = "/write")
+    public ResponseEntity<ContentDTO> save(@RequestPart ContentDTO contentDTO) {
         return new ResponseEntity<ContentDTO>(toDoService.save(contentDTO), HttpStatus.OK);
     }
 }
